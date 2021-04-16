e15ca2b72f488d5624c81f0fff0ece01cfc752f4
yongsannnn <yong_s@live.com>
2021-04-16 02:46:57 +0000

    Initial commit with react-router-dom and bootstrap added

---
+++ tgc-react/src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="container">
+        <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
