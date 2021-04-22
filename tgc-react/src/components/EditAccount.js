8aa3454ca052d04d9f19843ad1b9e603eef4c897
yongsannnn <yong_s@live.com>
2021-04-22 03:51:50 +0000

    Added css for editing account

--- tgc-react/src/components/EditAccount.js
+++ tgc-react/src/components/EditAccount.js
@@ -20,10 +20,10 @@ export default function EditAccount() {
     const [changeAddress, setChangeAddress] = useState(false)
     const [newAddress, setNewAddress] = useState("")
     const [checkUser, setCheckUser] = useState(false)
-
+    const [addressLength, setAddressLength] = useState(false)
     useEffect(() => {
         // Check if user_id and local storage id is the same. If same then allow user to proceed looking at the page. 
-        if (user_id){
+        if (user_id) {
             const fetch = async () => {
                 const response = await axios.get(baseUrl + "/api/users/edit/" + user_id)
                 setName(response.data.name)
@@ -35,7 +35,7 @@ export default function EditAccount() {
                 setCheckUser(true)
             }
             fetch()
-        } else { 
+        } else {
             setIsLoaded(true)
         }
         // eslint-disable-next-line
@@ -45,88 +45,113 @@ export default function EditAccount() {
         return (
             <div>Loading</div>
         )
-    } else if (isLoaded === true && checkUser === false){
+    } else if (isLoaded === true && checkUser === false) {
         return (
-            <div>Unauthorised access</div> 
+            <div>Unauthorised access</div>
         )
     } else {
         return (
             <React.Fragment>
-                <p>Editing Account {user_id}</p>
+                <div className="page-width">
+                    <div className="login-wrapper">
+                        <h1>Profile</h1>
 
-                <div>
-                    <p>{name}</p>
-                    <p>{email}</p>
-                    <p>{date}</p>
-                    <p>{phone}</p>
-                    <p>{address}</p>
-                    {/* <input type="text" placeholder="Name" name="name" value={name} onChange={(e) => setName(e.target.value)}></input>
-                <input type="text" placeholder="Email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-                <input type="date" placeholder="Date of Birth" name="date" value={date} onChange={(e) => setDate(e.target.value)}></input>
-                <input type="number" placeholder="Phone Number" name="phone" value={phone} onChange={(e) => setPhone(e.target.value)}></input>
-                <input type="text" placeholder="Address" name="address" value={address} onChange={(e) => setAddress(e.target.value)}></input> */}
-                </div>
-                <button onClick={
-                    () => {
-                        if (changePassword === true) {
-                            setChangePassword(false)
-                        } else {
-                            setChangePassword(true)
-                        }
-                    }
-                }>Change Password</button>
-                <div style={{
-                    display: changePassword === true ? "block" : "none"
-                }}>
-                    <input type="password" placeholder="New Password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-                    <input type="password" placeholder="Confirm New Password" name="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></input>
-                    <p style={{ display: isPasswordSame === true ? "block" : "none" }}>Password is not the same!! </p>
-
-                    <button onClick={
-                        async () => {
-                            if (password !== confirmPassword) {
-                                setIsPasswordSame(true)
-                            } else {
-                                const response = await axios.post(baseUrl + "/api/users/edit/" + user_id, {
-                                    "password": password
-                                })
-                                if (response.data === "Password Updated") {
-                                    history.push("/")
-                                } else {
-                                    console.log(response.data)
+                        <div>
+                            <table className="table">
+                                <tr>
+                                    <td className="edit-row">Name: </td>
+                                    <td>{name}</td>
+                                </tr>
+                                <tr>
+                                    <td className="edit-row">Email: </td>
+                                    <td>{email}</td>
+                                </tr>
+                                <tr>
+                                    <td className="edit-row">Date of Birth: </td>
+                                    <td>{date}</td>
+                                </tr>
+                                <tr>
+                                    <td className="edit-row">Contact Number: </td>
+                                    <td>{phone}</td>
+                                </tr>
+                                <tr>
+                                    <td className="edit-row">Address: </td>
+                                    <td>{address}</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div className="mb-2">
+                            <button className="cta mb-3" onClick={
+                                () => {
+                                    if (changePassword === true) {
+                                        setChangePassword(false)
+                                    } else {
+                                        setChangePassword(true)
+                                    }
                                 }
-                            }
-                        }
-                    }>Update Password</button>
-                </div>
-                 <button onClick={
-                    () => {
-                        if (changeAddress === true) {
-                            setChangeAddress(false)
-                        } else {
-                            setChangeAddress(true)
-                        }
-                    }
-                }>Change Address</button>
-                <div style={{
-                    display: changeAddress === true ? "block" : "none"
-                }}>
-                    <input type="text" placeholder="New Address" name="newAddress" onChange={(e) => setNewAddress(e.target.value)}></input>
-                    <button onClick={
-                        async () => {
-                            const response = await axios.post(baseUrl+"/api/users/edit/" + user_id,{
-                                "address": newAddress
-                            })
-                            if (response.data === "Address Updated"){
-                                history.push("/")
-                            } else {
-                                console.log(response.data)
-                            }
-                        }
-                    }>Update Address</button>
-                </div>
+                            }>Change Password</button>
+                            <div style={{
+                                display: changePassword === true ? "block" : "none"
+                            }}>
+                                <input className="login-input" type="password" placeholder="New Password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                                <input className="login-input" type="password" placeholder="Confirm Password" name="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></input>
+                                <p className="warning-text" style={{ display: isPasswordSame === true ? "block" : "none" }}>*Password does not match. </p>
 
+                                <button className="cta" onClick={
+                                    async () => {
+                                        if (password !== confirmPassword) {
+                                            setIsPasswordSame(true)
+                                        } else {
+                                            const response = await axios.post(baseUrl + "/api/users/edit/" + user_id, {
+                                                "password": password
+                                            })
+                                            if (response.data === "Password Updated") {
+                                                history.push("/")
+                                            } else {
+                                                console.log(response.data)
+                                            }
+                                        }
+                                    }
+                                }>Update Password</button>
+                            </div>
+                        </div>
+                        <div>
+                            <button className="cta mb-3" onClick={
+                                () => {
+                                    if (changeAddress === true) {
+                                        setChangeAddress(false)
+                                    } else {
+                                        setChangeAddress(true)
+                                    }
+                                }
+                            }>Change Address</button>
+                            <div style={{
+                                display: changeAddress === true ? "block" : "none"
+                            }}>
+                                <input className="login-input" type="text" placeholder="New Address" name="newAddress" onChange={(e) => setNewAddress(e.target.value)}></input>
+                                <p className="warning-text" style={{ display: addressLength === true ? "block" : "none" }}>*Address length is too long. Length must be shorter than 255 characters.</p>
+                                
+                                <button className="cta" onClick={
+                                    async () => {
+                                        if (newAddress.length > 255){
+                                            setAddressLength(true)
+                                        } else {
+                                            const response = await axios.post(baseUrl + "/api/users/edit/" + user_id, {
+                                                "address": newAddress
+                                            })
+                                            if (response.data === "Address Updated") {
+                                                history.push("/")
+                                            } else {
+                                                console.log(response.data)
+                                            }
+                                        }
+                                    }
+                                }>Update Address</button>
+                            </div>
+                        </div>
 
+                    </div>
+                </div>
             </React.Fragment>
 
         )
