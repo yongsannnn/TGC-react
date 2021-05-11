e4591450d75e7e99b53bf70eeaaeddb7af134d18
yongsannnn <yong_s@live.com>
2021-05-11 12:55:35 +0000

    Adjusted nav bar for mobile friendly

--- src/App.js
+++ src/App.js
@@ -24,14 +24,14 @@ function App() {
     const [loggedIn, setLoggedIn] = useState(false)
     const [userId, setUserId] = useState(0)
 
-    useEffect(()=>{
+    useEffect(() => {
         //Set interval for refresh token
-        setInterval (async () => {
+        setInterval(async () => {
             const response = await axios.post(baseUrl + "/api/users/refresh", {
                 "refreshToken": localStorage.getItem("refreshToken"),
             })
             localStorage.setItem("accessToken", response.data.accessToken)
-        }, 10 * 60 * 1000) 
+        }, 10 * 60 * 1000)
     }, [])
     // Check if there is token in local storage
     const isToken = localStorage.getItem("accessToken")
@@ -71,51 +71,65 @@ function App() {
         }
     }
 
+    const logoutUser = async () => {
+        const response = await axios.post(baseUrl + "/api/users/logout", {
+            "refreshToken": localStorage.getItem("refreshToken")
+        })
+        if (response.data) {
+            localStorage.clear()
+            setLoggedIn(false)
+        }
+    }
+
     return (
         <React.Fragment>
             <Router>
                 <ScrollToTop>
-                    <nav className="navbar navbar-expand-lg navbar-light" style={{ padding: "0rem 2rem" }}>
-                        <Link className="navbar-brand" style={{display: "flex", height: "40px"}}to="/">
+                    <nav className="navbar navbar-light" style={{ padding: "0rem 2rem" }}>
+                        <Link className="navbar-brand" style={{ display: "flex", height: "40px" }} to="/">
                             <img src={require("./leaf.png").default} alt="logo" /> <p className="logo-title">Tea Empire</p>
                         </Link>
-                        {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
-                            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button> */}
-                        {/* <div className="collapse navbar-collapse" id="navbarNavAltMarkup"> */}
-                        <div className="navbar-nav">
-                            <Link className="nav-item nav-link" to="/products">All Teas</Link>
-                            {/* <Link className="nav-item nav-link" to="/education">Education</Link> */}
+                        <div className="dropdown d-block d-sm-none">
+                            <span><i className="far fa-caret-square-down"></i></span>
+                            <div className="dropdown-content">
+                                <Link className="dropdown-link" to="/products">All Tea</Link>
+                                <Link className="dropdown-link" style={{
+                                    display: loggedIn === false ? "block" : "none"
+                                }} to="/login">Log In</Link>
+                                <Link className="dropdown-link" style={{
+                                    display: loggedIn === true ? "block" : "none"
+                                }} to={"/cart"}>Cart</Link>
+                                <Link className="dropdown-link" style={{
+                                    display: loggedIn === true ? "block" : "none"
+                                }} to={"/order"}>Orders</Link>
+                                <Link className="dropdown-link" style={{
+                                    display: loggedIn === true ? "block" : "none"
+                                }} to={"/edit/"}>Edit</Link>
+                                <Link className="dropdown-link" style={{
+                                    display: loggedIn === true ? "block" : "none"
+                                }} to="/" onClick={logoutUser}>Log Out</Link>
+                            </div>
                         </div>
-                        <div className="navbar-nav ml-auto">
+                        <div className="navbar-nav d-none d-md-block">
+                            <Link className="nav-item nav-link" to="/products">All Teas</Link>
+                        </div>
+                        <div className="navbar-nav ml-auto right-nav-bar d-none d-md-flex">
                             <Link className="nav-item nav-link ml-auto" style={{
                                 display: loggedIn === false ? "block" : "none"
                             }} to="/login">Log In</Link>
-                            <Link className="nav-item nav-link ml-auto" style={{
+                            <Link className="nav-item nav-link ml-auto mr-2" style={{
                                 display: loggedIn === true ? "block" : "none"
                             }} to={"/cart"}>Cart</Link>
-                            <Link className="nav-item nav-link ml-auto" style={{
+                            <Link className="nav-item nav-link ml-auto mr-2" style={{
                                 display: loggedIn === true ? "block" : "none"
                             }} to={"/order"}>Orders</Link>
-                            <Link className="nav-item nav-link ml-auto" style={{
+                            <Link className="nav-item nav-link ml-auto mr-2" style={{
                                 display: loggedIn === true ? "block" : "none"
                             }} to={"/edit/"}>Edit</Link>
                             <Link className="nav-item nav-link ml-auto" style={{
                                 display: loggedIn === true ? "block" : "none"
-                            }} to="/" onClick={
-                                async () => {
-                                    const response = await axios.post(baseUrl + "/api/users/logout", {
-                                        "refreshToken": localStorage.getItem("refreshToken")
-                                    })
-                                    if (response.data) {
-                                        localStorage.clear()
-                                        setLoggedIn(false)
-                                    }
-                                }
-                            }>Log Out</Link>
+                            }} to="/" onClick={logoutUser}>Log Out</Link>
                         </div>
-                        {/* </div> */}
                     </nav>
                     <section style={{
                         minHeight: "78vh"
@@ -160,7 +174,7 @@ function App() {
                     <Footer />
                 </ScrollToTop>
             </Router>
-        </React.Fragment>
+        </React.Fragment >
     );
 }
 
